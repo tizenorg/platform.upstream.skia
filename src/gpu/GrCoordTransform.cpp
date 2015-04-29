07be9f3ccf7379a14d1a6256ee339c1781213e2c
frederic.ma <frederic.ma@samsung.com>
2015-04-29 21:19:21 +0100

    Use fixed GLSL shader precision for texture sampling for GPU raster mode
    The purpose of this patch is to avoid extra shader compilation/linking
    by using the same GLSL precision in GPU raster mode.
    Current behavior dynamically changes the precision based on the texture size.
    During zoom in scenario, the precision changes from mediump to highp
    and generates an extra version of the fragment shader code.
    This extra shader incurs linking penalty and causes frame drops.
    The patch always uses the same precision (from highp to lowp order)
    to prevent generating another version of the shader for another precision.
    
    Some meaningful TryBot results:
    - power.samsung_1min(per_hour): 90.45 -53.6 mWh
    - power.samsung_thermal_scenario(per hour): 88.23 -113.9 mWh
    - irt_double_tap_zoom_out: 226.28 -3.1 ms
    - irt_pinch_zoom_in: 102.54 -3.6 ms
    - irt_pinch_zoom_out: 93.10 -9.5 ms
    - key_lagging: 203.13 -54.9 ms
    
    Bug: http://web.sec.samsung.net/bugzilla/show_bug.cgi?id=12855
    Reviewed by: Heejin Chung, Eero Häkkinen
    
    Change-Id: I90a9ddf131dd5aad367aaeb2b67834b6dfd219ce
    Signed-off-by: frederic.ma <frederic.ma@samsung.com>

--- src/gpu/GrCoordTransform.cpp
+++ src/gpu/GrCoordTransform.cpp
@@ -19,35 +19,22 @@ void GrCoordTransform::reset(GrCoordSet sourceCoords, const SkMatrix& m, const G
     fMatrix = m;
     fReverseY = kBottomLeft_GrSurfaceOrigin == texture->origin();
 
-    // Always start at kDefault. Then if precisions differ we see if the precision needs to be
-    // increased. Our rule is that we want at least 4 subpixel values in the representation for
-    // coords between 0 to 1 when bi- or tri-lerping and 1 value when nearest filtering. Note that
-    // this still might not be enough when drawing with repeat or mirror-repeat modes but that case
-    // can be arbitrarily bad.
-    int subPixelThresh = filter > GrTextureParams::kNone_FilterMode ? 4 : 1;
+    // take highest supported precision possible to avoid generating different
+    // version of shader code which triggers extra compilation/linking.
     fPrecision = kDefault_GrSLPrecision;
     if (texture->getContext()) {
         const GrShaderCaps* caps = texture->getContext()->caps()->shaderCaps();
         if (caps->floatPrecisionVaries()) {
-            int maxD = SkTMax(texture->width(), texture->height());
             const GrShaderCaps::PrecisionInfo* info;
-            info = &caps->getFloatShaderPrecisionInfo(kFragment_GrShaderType, fPrecision);
+            GrSLPrecision nextP = kHigh_GrSLPrecision;
             do {
-                SkASSERT(info->supported());
-                // Make sure there is at least 2 bits of subpixel precision in the range of
-                // texture coords from 0.5 to 1.0.
-                if ((2 << info->fBits) / maxD > subPixelThresh) {
-                    break;
-                }
-                if (kHigh_GrSLPrecision == fPrecision) {
-                    break;
-                }
-                GrSLPrecision nextP = static_cast<GrSLPrecision>(fPrecision + 1);
                 info = &caps->getFloatShaderPrecisionInfo(kFragment_GrShaderType, nextP);
-                if (!info->supported()) {
+                if (info->supported()) {
+                    fPrecision = nextP;
                     break;
                 }
-                fPrecision = nextP;
+                SkASSERT(nextP > kLow_GrSLPrecision);
+                nextP = static_cast<GrSLPrecision>(nextP - 1);
             } while (true);
         }
     }
